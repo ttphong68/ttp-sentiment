360e53ef6aaf04501a9b8f9776660a7302431351
ttphong68 <106058535+ttphong68@users.noreply.github.com>
2023-04-20 05:50:23 +0700

    Add files via upload

--- Library_Functions.py
+++ Library_Functions.py
@@ -67,11 +67,11 @@ import urllib.request as urllib2
 import warnings
 warnings.filterwarnings("ignore")
 #----------------------------------------------------------------------------------------------------
-import openai
-# link tạo API Key đó https://beta.openai.com/account/api-keys
-openai.organization = 'org-Vf0cOHTHl3VyD7bUQSqDmglv'
-# link lấy Organization ID https://beta.openai.com/account/org-settings
-openai.api_key = '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'
+# import openai
+# # link tạo API Key đó https://beta.openai.com/account/api-keys
+# openai.organization = 'org-Vf0cOHTHl3VyD7bUQSqDmglv'
+# # link lấy Organization ID https://beta.openai.com/account/org-settings
+# openai.api_key = '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'
 #------------------------------------------------------------------------------------------------------------------
 def dung():
     text_to_speech("Hẹn gặp lại bạn sau!")
