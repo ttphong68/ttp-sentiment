ebfa8bb382cb8e71367816a98398134af5679ed7
ttphong68 <106058535+ttphong68@users.noreply.github.com>
2023-04-20 17:05:06 +0700

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -413,9 +413,9 @@ elif choice == 'Dự đoán mới':
         def record_audio():
             CHUNK = 1024
             FORMAT = pyaudio.paInt16
-            CHANNELS = 1
+            CHANNELS = 2
             RATE = 44100
-            RECORD_SECONDS = 5
+            RECORD_SECONDS = 5 # Số giấy ghi ấm
             WAVE_OUTPUT_FILENAME = "output.wav"
 
             audio = pyaudio.PyAudio()
